b7401ec82762574c5090bb9e55bff3960be63fcc
pkb <pkb@kakao.com>
2022-07-23 00:23:45 +0900

    Some fixes and changes

--- pages/help.tsx
+++ pages/help.tsx
@@ -7,7 +7,7 @@ const Help: NextPage = () => (
   <Layout>
     <Container>
       <Title>Welcome to MC Game Stats.</Title>
-      <span>You can look up Hypixel players' stats with MC Game Stats.</span>
+      <span>You can look up Hypixel players{'\''} stats with MC Game Stats.</span>
       <span>Currently you can check Hypixel, Bedwars, and SkyWars level.</span>
       <span>This website uses <A target={'_blank'} href={'https://mc-heads.net/'}>MCHeads</A> for player avatars,
       <br />and <A target={'_blank'} href={'https://api.hypixel.net/'}>Hypixel Public API</A> for user stats.</span>
