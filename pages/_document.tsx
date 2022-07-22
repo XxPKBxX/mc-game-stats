22257c77b6e1d93617886729e30c29935c1c6e94
pkb <pkb@kakao.com>
2022-07-23 00:27:28 +0900

    Icon fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ const MyDocument = () => (
       href={'/favicon.svg'} />
       <link
       rel={'stylesheet'}
-      href={'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional'} />
+      href={'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'} />
       <link
       href={'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard-dynamic-subset.css'}
       rel={'stylesheet'}
